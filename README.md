21c8ff26f0f3e1c2eba078cec86e6dd3dc403912
Jon Erik Solheim <jones@restdb.io>
2016-12-20 16:58:29 +0100

    README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # restdb-cli
-Deployment and dev command line tools
+
+Command line tool for restdb.io database applications.
+
+Command:
+```
+$ restdb-cli <options> <command>
+```
+
+Command List
+
+  help          Display help information about restdb-cli.                        
+  apikey        Full access apikey to your restdb.io server.                      
+  deploy        Deploy folder (recursive) to restdb.io server.                    
+  destination   Destination folder for deployment folder at the restdb.io server.
--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Starting ...");
+const commandLineArgs = require('command-line-args');
+const getUsage = require('command-line-usage');
+
+const sections = [
+  {
+    header: 'restdb.io - Plug And Play Database service',
+    content: 'Command line tool for restdb.io database applications.'
+  },
+  {
+    header: 'restdb-cli',
+    content: '$ restdb-cli <options> <command>'
+  },
+  {
+    header: 'Command List',
+    content: [
+      { name: 'help', summary: 'Display help information about restdb-cli.' },
+      { name: 'apikey', summary: 'Full access apikey to your restdb.io server.' },
+      { name: 'deploy', summary: 'Deploy folder (recursive) to restdb.io server.' },
+      { name: 'destination', summary: 'Destination folder for deployment folder at the restdb.io server.' }
+    ]
+  }
+]
+
+const usage = getUsage(sections)
+
+const optionDefinitions = [
+  { name: 'help' },
+  { name: 'apikey' },
+  { name: 'deploy', alias: 'd'},
+  { name: 'destination', alias: 'r'}
+]
+const options = commandLineArgs(optionDefinitions);
+var isEmpty = (Object.keys(options).length === 0 && options.constructor === Object);
+console.log(options, isEmpty);
+if (options.help || isEmpty) {
+  console.log(usage);
+  return;
+}
+
+console.log("Starting ...")
